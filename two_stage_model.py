4d1c12427c0b59cfc5ff9afaff398d5352feff3d
Arjun Mukerji <stormandstress@gmail.com>
2018-05-21 13:03:29 -0700

    cleaned up; removed old and now-misleading code and comments, improved docstrings, etc.

--- two_stage_model.py
+++ two_stage_model.py
@@ -10,8 +10,9 @@ import lmfit as lf
 
 def two_stage_parameters():
     """
-    Return the parameters of the two-stage model as a lmfit Parameters object.
-    This allows their values, ranges, and whetehr they are free or fixed to be specified.
+    Return the parameters of the two-stage model as an lmfit Parameters object.
+    This allows specifying their values, ranges, and
+    whether they are fixed or free to vary (i.e. to be fitted).
     """
     params = lf.Parameters()
     params.add('m', value=1.3, vary=False)
@@ -26,31 +27,36 @@ def two_stage_parameters():
     return params
 
 def stage1(cmpt_thiseye, cmpt_othereye, mask_thiseye, mask_othereye, m=1.3, S=1, w_xm=1, w_xd=1):
-    """First (pre-binocular-summation) stage of the two-stage model"""
+    """
+    First (pre-binocular-summation) stage of the two-stage model
+    """
     return (cmpt_thiseye**m)/(S + cmpt_thiseye + cmpt_othereye + w_xm*mask_thiseye + w_xd*mask_othereye)
 
 def two_stage_response_err(params, C_thiseye, C_othereye, X_thiseye, X_othereye):
     """
-    Two-stage model of contrast response with facilitation
+    Two-stage model of contrast response with facilitation (residuals)
+
+    Inputs:
     C_thiseye, C_othereye: target contrasts in the two eyes, in percent
     X_thiseye, X_othereye: mask/surround contrasts in the two eyes, in percent
-    n: number of trials in this condition
-    c: number of correct responses in this condition
 
     Inputs should be lists or pd.Series and should all be the same length.
 
-    Output is predicted % correct - provided % correct (c/n), that is, the error - hence this is to be minimized
+    Outputs:
+    k - responses, where
+    k is a model parameter proportional to the std. dev. of late additive noise, and
+    responses are calculated from the input target/mask contrasts and other model parameters
+
+    The detection threshold is found by solving for input contrasts such that response = k,
+    so minimizing k-responses (the output of this function) solves the equation,
+    allowing model paramters to be estimated from thresholds, or vice versa.
     """
-
-    from scipy.stats import norm
-
-    #print(params, C_thiseye, C_othereye, X_thiseye, X_othereye)
 
     m = params['m'] # stage 1 excitatory constant
     S = params['S'] # stage 1 suppressive constant
     w_xm = params['w_m'] # want to fit this - monocular suppression weight
     w_xd = params['w_d'] # want to fit this - interocular suppression weight
-    a = params['a'] # facilitation parameter
+    a = params['a'] # facilitation parameter (varies per-subject but not per-condition)
     k = params['k'] # incremental response needed to achieve specified performance (d')
     p = params['p'] # stage 2 excitatory exponent 
     q = params['q'] # stage 2 suppressive exponent
@@ -75,26 +81,13 @@ def two_stage_response_err(params, C_thiseye, C_othereye, X_thiseye, X_othereye)
         resp_tm = ((1 + a*binsum_mask)*(binsum_target**p))/(Z+binsum_target**q)
         responses[i] = resp_tm
 
-        # calculate some additional stuff that we could use
-        #dprimes[i] = resp_tm * k # not sure about this...
-        #predicted_pct_correct[i] = norm.cdf(dprimes[i])
-        #errors[i] = predicted_pct_correct[i]-(ncorrect/ntrials)
-        #likelihoods[i] = loglikelihood(ntrials, ncorrect, predicted_pct_correct[i])
-
     return k-responses
 
-def two_stage_thresh(thresh_param, C_othereye, X_thiseye, X_othereye, fitted_params): #threshs will be minimized
+def two_stage_thresh(thresh_param, C_othereye, X_thiseye, X_othereye, fitted_params):
+    """
+    This is simply a wrapper around two_stage_response_err(),
+    which returns the residual to be minimized to fit the two-stage model,
+    but using the target contrast as the parameter to fit, not the model parameters
+    """
     C_thiseye = thresh_param['C_thiseye'].value
     return two_stage_response_err(fitted_params, [C_thiseye], C_othereye, X_thiseye, X_othereye)
-
-# def loglikelihood(n, c, predicted_pct_correct):
-#     """
-#     Calculate log-likelihood of the parameters that predicted predicted_pct_correct for a condition with n trials and c corrects (observed)
-#     """
-#     if c==n or c==0 or predicted_pct_correct==0 or predicted_pct_correct==1:
-#         return 0
-#     else:
-#         c_pred = np.round(predicted_pct_correct*n)
-#         ll_pred = (c_pred * np.log(predicted_pct_correct)) + ((n-c_pred)*(np.log(1-predicted_pct_correct)))
-#         ll_obs = (c*np.log(c/n)) + ((n-c)*np.log(1-(c/n)))
-#         return ll_pred-ll_obs # since lf.minimize() minimizes the sum of squares, return a residual
